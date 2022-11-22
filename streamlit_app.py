b20f500a1b08bd80bd8dd15777e30f766b0ed6df
iamjaswanth <jaswanthsrm@gmail.com>
2022-11-22 16:32:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.text('🥗Kale, Spinach & Rocket Smoothie')
 st.text('🥑🍞Hard-Boiled Free-Range Egg')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 st.dataframe(my_fruit_list)
 
 
