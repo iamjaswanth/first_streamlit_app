f466cc979c70469175981aaa3d0feaa40161c0ba
iamjaswanth <jaswanthsrm@gmail.com>
2022-11-30 12:04:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-st.text("Hello from Snowflake:")
+st.header("Hello from Snowflake:")
 st.text(my_data_row)
 
 
