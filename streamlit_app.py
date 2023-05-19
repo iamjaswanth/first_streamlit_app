f55bdd1d286d246631f834827d5edc7be01dfc9c
iamjaswanth <jaswanthsrm@gmail.com>
2023-05-19 08:39:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,6 @@ st.text('🥣 Omega 3 & Blueberry Oatmeal')
 st.text('🥗Kale, Spinach & Rocket Smoothie')
 st.text('🥑🍞Hard-Boiled Free-Range Egg')
 my_fruit_list = pd.read_csv("./first_streamlit_app/ind_niftymicrocap250_list.csv") 
-#my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-import pandas as pd
-import streamlit as st
-
-df = pd.read_csv("./data/titanic.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
-# df = pd.read_excel(...)  # will work for Excel files
-
-st.title("Hello world!")  # add a title
 st.write(df) 
 
 
