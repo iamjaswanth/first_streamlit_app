f77baaa334fba5eea0a73cafed10ba208d12c868
iamjaswanth <jaswanthsrm@gmail.com>
2023-05-19 10:05:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ rs_df = rs_df[rs_df.RS_Rating >= rs_df.RS_Rating.quantile(.70)]
 # Checking Minervini conditions of top 30% of stocks in given list
 rs_stocks = rs_df['Ticker']
 for stock in rs_stocks:
-    st.write(stock)
+   
     try:
 
         df = pdr.get_data_yahoo(stock + '.NS', start_date, end_date)
@@ -111,7 +111,7 @@ for stock in rs_stocks:
 
         if(condition_1 and condition_2 and condition_3 and condition_4 and condition_5 and condition_6 and condition_7 and condition_8 and condition_9):
             exportList = exportList.append({'Stock': stock, "RS_Rating": RS_Rating ,"20 Day MA": moving_average_20,"50 Day MA": moving_average_50, "150 Day Ma": moving_average_150, "200 Day MA": moving_average_200, "52 Week Low": low_of_52week, "52 week High": high_of_52week,'CP':CP}, ignore_index=True)
-            print (stock + " made the Minervini requirements")
+            st.write(stock + " made the Minervini requirements")
     except Exception as e:
         print (e)
         print(f"Could not gather data on {stock}")
