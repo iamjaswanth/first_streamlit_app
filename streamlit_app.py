5a58cc82f2ba5f2fc77336329676ed4064e3104d
iamjaswanth <jaswanthsrm@gmail.com>
2023-05-19 09:17:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,9 @@ for ticker in tickers['Symbol']:
         print (f'Ticker: {ticker}; Returns Multiple against NIFTY50: {returns_multiple}\n')
     except:
          print(f"Could not gather data on {ticker}")
+            
+            
+  
          
     
    
@@ -48,7 +51,85 @@ for ticker in tickers['Symbol']:
 rs_df = pd.DataFrame(list(zip(tickers['Symbol'], returns_multiples)), columns=['Ticker', 'Returns_multiple'])
 rs_df['RS_Rating'] = rs_df.Returns_multiple.rank(pct=True) * 100
 rs_df = rs_df[rs_df.RS_Rating >= rs_df.RS_Rating.quantile(.70)]
-st.write(rs_df) 
+
+
+# Checking Minervini conditions of top 30% of stocks in given list
+rs_stocks = rs_df['Ticker']
+for stock in rs_stocks:
+    print(stock)
+    try:
+
+        df = pdr.get_data_yahoo(stock + '.NS', start_date, end_date)
+        sma = [20,50, 150, 200]
+        for x in sma:
+            df["SMA_"+str(x)] = round(df['Adj Close'].rolling(window=x).mean(), 2)
+        
+        # Storing required values
+        currentClose = df["Adj Close"][-1]
+        moving_average_20 = df["SMA_20"][-1]
+        moving_average_50 = df["SMA_50"][-1]
+        moving_average_150 = df["SMA_150"][-1]
+        moving_average_200 = df["SMA_200"][-1]
+        low_of_52week = round(min(df["Low"][-252:]), 2)
+        high_of_52week = round(max(df["High"][-252:]), 2)
+        RS_Rating = round(rs_df[rs_df['Ticker']==stock].RS_Rating.tolist()[0])
+        
+        try:
+            moving_average_200_20 = df["SMA_200"][-20]
+        except Exception:
+            moving_average_200_20 = 0
+        # Condition 1: Current Price > 150 SMA and > 200 SMA
+        condition_1 = currentClose > moving_average_150 > moving_average_200
+    
+        # Condition 2n: 150 SMA and > 200 SMA
+        condition_2 = moving_average_150 > moving_average_200
+
+        # Condition 3: 200 SMA trending up for at least 1 month
+        condition_3 = moving_average_200 > moving_average_200_20
+        
+        # Condition 4: 50 SMA> 150 SMA and 50 SMA> 200 SMA
+        condition_4 = moving_average_50 > moving_average_150 > moving_average_200
+           
+        # Condition 5: Current Price > 50 SMA
+        condition_5 = currentClose > moving_average_50
+           
+        # Condition 6: Current Price is at least 30% above 52 week low
+        condition_6 = currentClose >= (1.3*low_of_52week)
+           
+        # Condition 7: Current Price is within 25% of 52 week high
+        condition_7 = currentClose >= (.75*high_of_52week)
+        
+        condition_8 = currentClose < 10000
+        
+        
+        # Condition 5: Current Price > 50 SMA
+        condition_9 = currentClose > moving_average_20
+        
+
+         
+        
+        CP = currentClose
+
+
+        if(condition_1 and condition_2 and condition_3 and condition_4 and condition_5 and condition_6 and condition_7 and condition_8 and condition_9):
+            exportList = exportList.append({'Stock': stock, "RS_Rating": RS_Rating ,"20 Day MA": moving_average_20,"50 Day MA": moving_average_50, "150 Day Ma": moving_average_150, "200 Day MA": moving_average_200, "52 Week Low": low_of_52week, "52 week High": high_of_52week,'CP':CP}, ignore_index=True)
+            print (stock + " made the Minervini requirements")
+    except Exception as e:
+        print (e)
+        print(f"Could not gather data on {stock}")
+
+exportList = exportList.sort_values(by='RS_Rating', ascending=False)
+
+print(exportList.iloc[:25])
+
+MomentumStocks = exportList.iloc[:25][['Stock', 'CP']]
+
+MomentumStocks['weights'] = [1/len(MomentumStocks)] * len(MomentumStocks)
+
+MomentumStocks['Quantity'] = (MomentumStocks['CP'].max()/MomentumStocks['CP'])
+
+MomentumStocks['Puschare Value'] = MomentumStocks['Quantity'] * MomentumStocks['CP']
+st.write(MomentumStocks) 
 
 
 
