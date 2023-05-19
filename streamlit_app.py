4c0987d76a83c1f7af91f6e7bac8733e71556d8b
iamjaswanth <jaswanthsrm@gmail.com>
2023-05-19 08:40:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 st.text('🥣 Omega 3 & Blueberry Oatmeal')
 st.text('🥗Kale, Spinach & Rocket Smoothie')
 st.text('🥑🍞Hard-Boiled Free-Range Egg')
-my_fruit_list = pd.read_csv("./first_streamlit_app/ind_niftymicrocap250_list.csv") 
+my_fruit_list = pd.read_csv("./ind_niftymicrocap250_list.csv") 
 st.write(df) 
 
 
