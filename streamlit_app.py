85f8ea773511c6feb844d1231f9307f3be0568a4
iamjaswanth <jaswanthsrm@gmail.com>
2023-05-19 10:19:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -110,7 +110,6 @@ for stock in rs_stocks:
         
         CP = currentClose
 
-        st.write(CP)
         if(condition_1 and condition_2 and condition_3 and condition_4 and condition_5 and condition_6 and condition_7 and condition_8 and condition_9):
              exportList = exportList.append({'Stock': stock, "RS_Rating": RS_Rating ,"20 Day MA": moving_average_20,"50 Day MA": moving_average_50, "150 Day Ma": moving_average_150, "200 Day MA": moving_average_200, "52 Week Low": low_of_52week, "52 week High": high_of_52week,'CP':CP}, ignore_index=True)
              st.write(exportList)
